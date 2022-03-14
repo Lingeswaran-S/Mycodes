afb9b032616841c4d0ceca53e219df81d99ff9fc
lingeswaran <49890518+Lingeswaran-S@users.noreply.github.com>
2022-03-14 12:55:03 +0530

    others changes

--- App.java
+++ App.java
@@ -2,6 +2,6 @@ class App{
 	public static void main(String[] args){
 		system.out.println("Hi how are you?");
 		system.out.println("fine");
-		system.out.println("fine");
+		system.out.println("Some develpoer change code");
 	}
 }
